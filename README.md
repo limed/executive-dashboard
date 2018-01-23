f24b6eae5c6757e667a5218d6ac46af6538777fb
limed <limed@mozilla.com>
2018-01-23 14:29:37 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Executive dashboard
+This repo contains a couple of top level executive dashboard like things. This repo will need to be built out of the nubis-market account since that is where we have the aggregrate prometheus data
+
+
---
+++ nubis/puppet/grafana.pp
@@ -0,0 +1,65 @@
+class { 'grafana':
+  install_method => 'repo',
+  version        => '4.6.3',
+  cfg            => {
+    app_mode          => 'production',
+    'server'          => {
+      protocol => 'http',
+      root_url => '/exec-grafana',
+    },
+    'auth.anonymous'  => {
+      enabled => true,
+    },
+    # Needs to be disabled for traefik, enabled for grafana_datasource, hurgh
+    'auth.basic'      => {
+      enabled => true,
+    },
+    'auth.proxy'      => {
+      enabled         => true,
+      header_name     => 'OIDC_CLAIM_email',
+      header_property => 'email',
+      auto_sign_up    => true,
+    },
+    users             => {
+      allow_sign_up        => true,
+      auto_assign_org      => true,
+      auto_assign_org_role => 'Editor',
+    },
+    'dashboards.json' => {
+      enabled => true,
+    },
+  },
+}->
+exec { 'wait-for grafana startup':
+  command => '/bin/sleep 15',
+}->
+grafana_datasource { 'prometheus':
+  grafana_url      => 'http://localhost:3000',
+  grafana_user     => 'admin',
+  grafana_password => 'admin',
+  type             => 'prometheus',
+  url              => 'http://localhost:81/prometheus',
+  access_mode      => 'proxy',
+  is_default       => true,
+}
+
+file { '/var/lib/grafana/dashboards':
+  ensure  => directory,
+  owner   => grafana,
+  group   => grafana,
+  mode    => '0640',
+  recurse => true,
+  purge   => true,
+  source  => 'puppet:///nubis/files/grafana/dashboards',
+  require => [
+    Class['grafana'],
+  ]
+}
+
+file { '/etc/consul/svc-exec-dashboard.json':
+	ensure => file,
+  owner  => root,
+  group  => root,
+  mode   => '0644',
+  source => 'puppet:///nubis/files/svc-exec-dashboard.json',
+}
